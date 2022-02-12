387e9d6fb26ebe34faa5aec11d3295c23518acd6
Raphael Furtunato <rfurtunato@lely.com>
2022-02-12 10:11:54 +0100

    Implement send command to runner

--- lua/init.lua
+++ lua/init.lua
@@ -1,3 +1,4 @@
+local tmux = require("lua.tmux")
 local vim = vim
 local M = {
 }
@@ -5,6 +6,9 @@ local M = {
 P = function(v)
     print(vim.inspect(v))
     return v
+end
+
+M.setup = function(opts)
 end
 
 M.send_command = function(cmd)
@@ -16,43 +20,6 @@ M.send_command = function(cmd)
   return s
 end
 
-M.setup = function(opts)
-end
-
-M.tmux_panes = function()
-    local panes = M.send_tmux_command("list-panes")
-    print(panes)
-    local split_panes = vim.split(panes, "\n")
-    P(split_panes)
-    return split_panes
-end
-
-M.prompt_for_pane_to_attach = function()
-    os.execute("tmux display-panes")
-end
-
-M.send_tmux_command = function(command)
-    return M.send_command("tmux " .. command)
-end
-
-M.attach_pane = function()
-    local numb_panes = tonumber(M.send_command("tmux display-message -p '#{window_panes}'"))
-    if numb_panes == 1 then
-        print("create new pane")
-    elseif numb_panes == 2 then
-        -- TODO auto attach_pane
-        M.prompt_for_pane_to_attach()
-        local pane_to_attach = tonumber(vim.fn.input('Enter pane number: '))
-        M["pane"] = pane_to_attach
-        P(M)
-    else
-        M.prompt_for_pane_to_attach()
-        local pane_to_attach = tonumber(vim.fn.input('Enter pane number: '))
-        M["pane"] = pane_to_attach
-        P(M)
-    end
-end
-
 M.run_behave = function()
     print("Hello world")
 end
@@ -62,5 +29,5 @@ M.debug_behave = function()
 end
 
 -- M.attach_pane()
-M.tmux_panes()
+tmux.tmux_panes()
 return M
---
+++ lua/tmux.lua
@@ -0,0 +1,52 @@
+Tmux = {}
+
+Tmux.send_command = function(cmd)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  return s
+end
+
+Tmux.tmux_panes = function()
+    local panes = Tmux.send_tmux_command("list-panes")
+    print(panes)
+    local split_panes = vim.split(panes, "\n")
+    P(split_panes)
+    return split_panes
+end
+
+Tmux.send_command_to_runner = function(cmd)
+    os.execute("tmux send-keys -t " .. Tmux["pane"] .. " \"".. cmd .. "\" Enter")
+end
+
+Tmux.prompt_for_pane_to_attach = function()
+    os.execute("tmux display-panes")
+end
+
+Tmux.send_tmux_command = function(command)
+    return Tmux.send_command("tmux " .. command)
+end
+
+Tmux.attach_pane = function()
+    local numb_panes = tonumber(Tmux.send_command("tmux display-message -p '#{window_panes}'"))
+    if numb_panes == 1 then
+        print("create new pane")
+    elseif numb_panes == 2 then
+        -- TODO auto attach_pane
+        Tmux.prompt_for_pane_to_attach()
+        local pane_to_attach = tonumber(vim.fn.input('Enter pane number: '))
+        Tmux["pane"] = pane_to_attach
+        P(Tmux)
+    else
+        Tmux.prompt_for_pane_to_attach()
+        local pane_to_attach = tonumber(vim.fn.input('Enter pane number: '))
+        Tmux["pane"] = pane_to_attach
+        P(Tmux)
+    end
+end
+
+Tmux.attach_pane()
+Tmux.send_command_to_runner("git status")
+return Tmux
